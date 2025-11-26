57932a82073af7df1f739c3131d48fbc599f01d0
DanielSolarte27 <danielsolarte27@gmail.com>
2025-11-26 12:02:58 -0500

    Remove unused SVG files: next.svg, vercel.svg, and window.svg

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -12,35 +13,6 @@ const nextConfig: NextConfig = {
         hostname: "api.dicebear.com",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "upgrade-insecure-requests",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
-      },
-    ];
   },
 };
 
